f1ee0f8efde5e4429c93b5e4fa35e4c6d2afa9ac
triplesix2001 <mr.bargum@hotmail.no>
2023-06-24 00:40:41 +0200

    Finished battery settings ( not properly tested )

--- main.py
+++ main.py
@@ -2,11 +2,42 @@ import eel
 import sys
 import ctypes
 import os
+import subprocess
+import traceback
 
 # Called by script.js, exposing my_python_func
 @eel.expose
 def my_python_function():
     print("Button clicked!")
+
+# Exposed function
+@eel.expose
+def disable_sleep():
+    try:
+        # Command to set monitor timeout on AC power
+        subprocess.run('powercfg /Change monitor-timeout-ac 60', shell=True, capture_output=True, text=True)
+        # Command to set monitor timeout on DC power
+        subprocess.run('powercfg /Change monitor-timeout-dc 0', shell=True, capture_output=True, text=True)
+        # Command to set standby timeout on AC power
+        subprocess.run('powercfg /Change standby-timeout-ac 0', shell=True, capture_output=True, text=True)
+        # Command to set standby timeout on DC power
+        subprocess.run('powercfg /Change standby-timeout-dc 0', shell=True, capture_output=True, text=True)
+        subprocess.run('powercfg /setActive scheme_current', shell=True, capture_output=True, text=True)
+        return "success"
+    except Exception as e:
+        traceback.print_exc()  # Print the exception traceback for debugging
+        return str(e)
+
+
+@eel.expose
+def disable_sleep_lid_close():
+    try:
+        subprocess.run('powercfg /SETACVALUEINDEX SCHEME_CURRENT SUB_BUTTONS LIDACTION 0', shell=True, capture_output=True, text=True)
+        subprocess.run('powercfg /setActive scheme_current', shell=True, capture_output=True, text=True)
+        return "success"
+    except Exception as e:
+        return str(e)
+
 
 # Init logic
 def main():
--- web/Windows.js
+++ web/Windows.js
@@ -11,15 +11,97 @@
 //    eel.my_python_function(); // Replace "my_python_function" with your actual Eel function name
 //});
 
-
-document.getElementById('data-collection-btn').addEventListener('click', function() {
-    // Add the "active" class to the info-div
+eel.expose(showMessage)
+function showMessage(message) {
     var popup = document.getElementById('popup-div');
-    popup.classList.add('active');
+    var successClass = 'success';
+    var errorClass = 'error';
+    var messageText = '';
   
-    // Remove the "active" class after 5 seconds
+    // Remove existing classes
+    popup.classList.remove(successClass, errorClass);
+  
+    // Determine the appropriate class and message text based on the message type
+    if (message === 'success') {
+      popup.classList.add(successClass);
+      messageText = 'Operation successful!';
+    } else if (message === 'error') {
+      popup.classList.add(errorClass);
+      messageText = 'An error occurred.';
+    }
+  
+    // Delay displaying the message to ensure it appears after the infobox is shown
     setTimeout(function() {
-      popup.classList.remove('active');
-    }, 5000);
+      // Insert the message text into the popup
+      popup.innerHTML = messageText;
+  
+      // Add the "active" class to display the popup
+      popup.classList.add('active');
+  
+      // Remove the "active" class after 5 seconds
+      setTimeout(function() {
+        popup.classList.remove('active');
+      }, 8000);
+    }, 100); // Adjust the delay as needed
+  }
+  
+  
+  
+
+// Data Collection Button
+document.getElementById('data-collection-btn').addEventListener('click', function() {
+    showMessage('success'); // Handle data collection button click
+  });
+  
+  // Telemetry Button
+  document.getElementById('telemetry-btn').addEventListener('click', function() {
+    showMessage('error');
+    eel.my_python_function(); // Handle telemetry button click
+  });
+  
+  // Turn Off Sleep Button
+  document.getElementById('turn-off-sleep-btn').addEventListener('click', async function() {
+    try {
+      const response = await eel.disable_sleep()();
+      console.log("success: " + response);
+      showMessage(response);
+    } catch (error) {
+      console.log("error: " + error);
+      showMessage("error");
+    }
+  });
+  
+
+  
+  // Disable Lid Sleep Button
+  document.getElementById('disable-lid-sleep-btn').addEventListener('click', async function() {
+    try {
+      const response = await eel.disable_sleep_lid_close()();
+      console.log("success: " + response);
+      showMessage(response);
+    } catch (error) {
+      console.log("error: " + error);
+      showMessage("error");
+    }
+  });
+  
+  // Disable Automatic Updates Button
+  document.getElementById('disable-automatic-updates-btn').addEventListener('click', function() {
+    // Handle disable automatic updates button click
+  });
+  
+  // Disable Windows 11 Button
+  document.getElementById('disable-win11-btn').addEventListener('click', function() {
+    // Handle disable Windows 11 button click
+  });
+  
+  // Crack Windows Button
+  document.getElementById('crack-btn').addEventListener('click', function() {
+    // Handle crack Windows button click
+  });
+  
+  // Make Local Account Button
+  document.getElementById('new-local-acc-btn').addEventListener('click', function() {
+    // Handle make local account button click
   });
   
